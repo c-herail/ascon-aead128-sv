e33b2f4abd2309cf18fbecd2e00947a3775c9db1
c-herail <cyril.herail@etu.emse.fr>
2025-10-02 00:17:23 +0200

    set reset to active-low

--- core/data_reg.sv
+++ core/data_reg.sv
@@ -5,23 +5,23 @@
  *      parameter(s):
  *        WIDTH : width of the input in bits
  *      input(s):
- *        - clk : clock
- *        - rst : asynchronous active-high reset
- *        - en  : enable signal
- *        - d   : input data
+ *        - clk   : clock
+ *        - rst_n : asynchronous active-low reset
+ *        - en    : enable signal
+ *        - d     : input data
  *      output(s):
  *        - q : output data
  ******************************************************************************/
 
 module data_reg #(parameter WIDTH = 32)(
     input  logic             clk,
-    input  logic             rst,
+    input  logic             rst_n,
     input  logic             en,
     input  logic [WIDTH-1:0] d,
     output logic [WIDTH-1:0] q );
 
-    always_ff @(posedge clk) begin
-        if (rst == 1'b1) begin
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
             q <= '0;
         end
         else begin
